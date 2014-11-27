d74edb20c497ee41ae341cc8ad6869e4a50505dc
fahchen <dev.fah@gmail.com>
2014-11-27 15:28:47 +0800

    Improve Invitation.generate
    [Issue #1] https://github.com/iec-uestc/campo/issues/1
    
            https://trello.com/c/vjLL1u9r
    
    Improved Invitation.generate so now it will generate a unique code

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,6 @@
 class Invitation < ActiveRecord::Base
+  validates :code, uniqueness: true
+
   belongs_to :inviter, class_name: 'User'
   belongs_to :invitee, class_name: 'User'
 
@@ -7,7 +9,9 @@ class Invitation < ActiveRecord::Base
   private
 
   def generate_code
-    self.code = SecureRandom.base64(6)
+    begin
+      self.code = SecureRandom.urlsafe_base64
+    end while self.class.where(code: self.code).any?
   end
 
 end
